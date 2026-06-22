89017469e3b2d4b78fe5b83bd82a59d66c3f4f99
Abishek-Tharu99 <hackerheaven73@gmail.com>
2026-06-22 18:40:27 +0545

    finally guys

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -8,7 +8,7 @@ import type {
 
 // const API_URL = import.meta.env.VITE_API_URL;
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_URL || "https://vedict-match.onrender.com";
 
 // console.log("API URL:", API_BASE_URL);
 
