d184afca09400c6b61fe55db646bb9586e838b6e
Abishek-Tharu99 <hackerheaven73@gmail.com>
2026-07-12 22:44:15 +0545

    fixing stilling

--- server/src/routes/blog.ts
+++ server/src/routes/blog.ts
@@ -15,13 +15,13 @@ const router = Router();
 // router.get("/", (_req, res) => {
 //   res.json({ message: "Blog route is working 🚀" });
 // });
+router.get("/id/:id", getBlogById);
 
 router.get("/", getBlogs);
 router.get("/:slug", getBlogBySlug);
 router.post("/", authenticate, createBlog);
 router.put("/:id", updateBlog);
 router.delete("/:id", deleteBlog);
-router.get("/id", getBlogById);
 
 
 export default router;
